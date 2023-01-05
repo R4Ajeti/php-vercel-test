3ce4854a822a5cd3e49ac54e0a2da76dac071eb2
Rinor Ajeti <r4ajeti@gmail.com>
2023-01-05 16:33:09 +0100

    WIP

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ echo we herere
 cat /etc/os-release
 node --version
 yum check-update
-yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 php --version
