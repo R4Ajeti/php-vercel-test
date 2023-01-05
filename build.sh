5ac29a060ec299767a5a3ccbf7039d4a6ec1d79a
Rinor Ajeti <r4ajeti@gmail.com>
2023-01-05 17:01:10 +0100

    WIP

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ yum install -y amazon-linux-extras
 amazon-linux-extras enable php8.1
 yum clean metadata
 yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
+php --version
  
 # # Install PHP & WGET
 # echo we herere
