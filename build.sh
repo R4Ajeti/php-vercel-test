c6c063321e56e357ac89523498c7038a43ef07b3
Rinor Ajeti <r4ajeti@gmail.com>
2023-01-05 16:30:15 +0100

    WIP

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 echo we herere
 cat /etc/os-release
 node --version
-yum dnf module list php
 yum check-update
-yum dnf module list php
+sudo yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
