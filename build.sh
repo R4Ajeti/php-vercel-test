6bfea279ecee5a85d6fac95eb8f25a3f989fcc32
Rinor Ajeti <r4ajeti@gmail.com>
2023-01-05 16:34:43 +0100

    WIP

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@ cat /etc/os-release
 node --version
 yum check-update
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+dnf -y module reset php
+dnf -y install dnf-plugins-core
+dnf config-manager --set-enabled remi
+dnf -y module install php:remi-8.1
 # yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 php --version
