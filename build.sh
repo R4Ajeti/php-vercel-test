7b1410fad39d58d580f674deb0c3325c9456a4e1
Rinor Ajeti <r4ajeti@gmail.com>
2023-01-05 16:59:29 +0100

    WIP

--- build.sh
+++ build.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
- 
-# Install PHP & WGET
-echo we herere
-cat /etc/os-release
-node --version
-yum update
-yum check-update
-yum install epel-release yum-utils
 yum install -y amazon-linux-extras
 amazon-linux-extras enable php8.1
 yum clean metadata
-# yum install http://rpms.remirepo.net/fedora/remi-release-36.rpm
-# yum install epel-release
-# yum install module list php
-# yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-# dnf -y module reset php
-# dnf -y install dnf-plugins-core
-# dnf config-manager --set-enabled remi
-# dnf -y module install php:remi-8.1
-# yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-php --version
+yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
+ 
+# # Install PHP & WGET
+# echo we herere
+# cat /etc/os-release
+# node --version
+# yum update
+# yum check-update
+# yum install -y amazon-linux-extras
+# amazon-linux-extras enable php8.1
+# yum clean metadata
+# # yum install http://rpms.remirepo.net/fedora/remi-release-36.rpm
+# # yum install epel-release
+# # yum install module list php
+# # yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# # dnf -y module reset php
+# # dnf -y install dnf-plugins-core
+# # dnf config-manager --set-enabled remi
+# # dnf -y module install php:remi-8.1
+# # yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+# php --version
